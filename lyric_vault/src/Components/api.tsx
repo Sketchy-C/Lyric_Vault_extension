3109aab21e40a5cc7cc80925f46c071a2db8f199
Sketchy-C <willy254dabs@gmail.com>
2025-03-04 09:12:06 -0800

    new

--- lyric_vault/src/Components/api.tsx
+++ lyric_vault/src/Components/api.tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const fetchTranscript = async (videoId: string) => {
   try {
-    const response = await axios.get(`https://lyric-vault.onrender.com/api/transcript/?videoId=${videoId}`);
+    const response = await axios.get(`http://127.0.0.1:8000/api/transcript//?videoId=${videoId}`);
     return response.data;
   } catch (err) {
     throw new Error('Failed to fetch transcript '+ err);
